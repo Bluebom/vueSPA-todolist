83797fe091b2d99d4709827319ca790198c82d74
Bluebom <Franklinhpf@hotmail.com>
2021-09-26 17:55:57 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import Vue from 'vue'
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
 Vue.config.productionTip = false
